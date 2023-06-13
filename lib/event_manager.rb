93024dea1e31f03a3743c7bf52582eda6871703c
Caleb Stein <caleb@calebstein.net>
2023-06-13 10:05:30 -0700

    added day of week counter

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,22 @@
+require 'pry-byebug'
 require 'csv'
 require 'erb'
+require 'date'
 require 'phone'
 require 'google/apis/civicinfo_v2'
 puts 'Event Manager Initialized!'
+
+DAYS = [
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday'
+]
+
+REGDAYS = []
 
 def validate_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -14,6 +28,14 @@ def validate_phone_number(phone)
 rescue
   "Invalid phone number"
 end 
+
+def peak_hours(t)
+  t = Time.parse(t)
+end
+
+def day_of_week(d)
+  d = DAYS[Date.strptime(d, "%m/%d/%y").wday]
+end
 
 def find_legislators(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
@@ -52,6 +74,16 @@ contents.each do |row|
 
   #generate_letter(id, form_letter)
 
-  puts row[:homephone]
-  puts validate_phone_number(row[:homephone])
+  #puts row[:homephone]
+  #puts validate_phone_number(row[:homephone])
+
+  #puts peak_hours(row[:regdate])
+  REGDAYS.push(day_of_week(row[:regdate]))
 end
+
+regs_per_day = REGDAYS.reduce(Hash.new(0)) do |regs_per_day, day|
+  regs_per_day[day] += 1
+  regs_per_day
+end
+
+puts regs_per_day
